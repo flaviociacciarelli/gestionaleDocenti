e38c5588e51217a8cbafb459fb61eeaafa4b3ab0
flaviociacciarelli <flavciac@gmail.com>
2025-07-14 15:05:21 +0200

    ciao

---
+++ assets/js/docenti.js
@@ -0,0 +1,34 @@
+fetch("http://localhost:3000/api/docenti")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("non sono riuscito a recuperare i dati");
+    }
+    return response.json();
+  })
+  .then((docenti) => {
+    const table = document.getElementById("tabellaDocenti");
+    docenti.forEach((docente) => {
+        console.log(docente);
+        
+      const row = document.createElement("tr");
+
+      row.innerHTML = `
+      <td class="col">${docente.infoPersonali.nome}</td>
+      <td class="col">${docente.infoPersonali.cognome}</td>
+      <td class="col">${docente.infoPersonali.email}</td>
+      <td class="col">${docente.infoPersonali.telefono}</td>
+      <td class="col">${docente.infoPersonali.dataNascita}</td>
+      <td class="col">${docente.infoPersonali.cf}</td>
+      <td class="col">${docente.lavoro.materiaInsegnata}</td>
+      <td class="col">${docente.lavoro.dataAssunzione}</td>
+      `;
+
+        table.appendChild(row);
+
+    });
+  })
+
+  .catch((error) => {
+    document.body.innerHTML += `<p style="color:red;">${error.message}</p>`;
+    console.error("errore nelle fetch", error);
+  });
