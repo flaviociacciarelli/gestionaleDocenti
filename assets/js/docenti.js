8638b47325aa17c9d70791fff23f3c4b8b35d9d7
flaviociacciarelli <flavciac@gmail.com>
2025-07-15 11:49:02 +0200

    aiuto

--- assets/js/docenti.js
+++ assets/js/docenti.js
@@ -7,29 +7,62 @@ fetch("http://localhost:3000/api/docenti")
   })
   .then((docenti) => {
     const table = document.getElementById("tabellaDocenti");
+
+    let maschi = 0;
+    let femmine = 0;
+    let altro = 0;
+
     docenti.forEach((docente) => {
-        console.log(docente);
-        
+      console.log(docente);
+
       const row = document.createElement("tr");
 
       row.innerHTML = `
-      <td class="col">${docente.infoPersonali.nome}</td>
-      <td class="col">${docente.infoPersonali.cognome}</td>
-    <td class="col">${docente.infoPersonali.sesso}</td>
-      <td class="col">${docente.infoPersonali.email}</td>
-      <td class="col">${docente.infoPersonali.telefono}</td>
-      <td class="col">${docente.infoPersonali.dataNascita}</td>
-      <td class="col">${docente.infoPersonali.cf}</td>
-      <td class="col">${docente.lavoro.materiaInsegnata}</td>
-      <td class="col">${docente.lavoro.dataAssunzione}</td>
+      <td class="tdNome">${docente.infoPersonali.nome}</td>
+      <td id="striped">${docente.infoPersonali.cognome}</td>
+    <td>${docente.infoPersonali.sesso}</td>
+      <td id="striped">${docente.infoPersonali.email}</td>
+      <td>${docente.infoPersonali.telefono}</td>
+      <td id="striped">${docente.infoPersonali.dataNascita}</td>
+      <td>${docente.infoPersonali.cf}</td>
+      <td id="striped">${docente.lavoro.materiaInsegnata}</td>
+      <td>${docente.lavoro.dataAssunzione}</td>
       `;
 
-        table.appendChild(row);
+      table.appendChild(row);
 
+      const sesso = docente.infoPersonali.sesso.toLowerCase();
+      if (sesso === "m") {
+        maschi += 1;
+      } else if (sesso === "f") {
+        femmine += 1;
+      } else {
+        altro += 1;
+      }
+    });
+
+    // Pie Chart (dopo il conteggio)
+    const ctxGender = document.getElementById("genderChart").getContext("2d");
+    new Chart(ctxGender, {
+      type: "pie",
+      data: {
+        labels: ["Maschi", "Femmine", "Altro"],
+        datasets: [
+          {
+            data: [maschi, femmine, altro],
+            backgroundColor: ["#36A2EB", "#FF6384", "#5D5D5D"],
+            borderColor: "#fff",
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        plugins: { legend: { position: "bottom" } },
+      },
     });
   })
-
   .catch((error) => {
     document.body.innerHTML += `<p style="color:red;">${error.message}</p>`;
-    console.error("errore nelle fetch", error);
+    console.error("errore nella fetch", error);
   });
